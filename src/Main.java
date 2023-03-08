1d837811649f1d52eae3074d198b3fd2ed15ffa4
Volgin7 <8612020@gmail.com>
2023-03-08 13:24:54 +0300

    +task2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         task1();
+        task2();
 
     }
 
@@ -27,5 +28,24 @@ public class Main {
         }
         return arr;
     }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] array = generateRandomArray();
+        int minPayment = array[0];
+        int maxPayment = array[0];
+
+        for(int payment:array) {
+            if (payment > maxPayment) {
+                maxPayment = payment;
+            }
+            if (payment < minPayment) {
+                minPayment = payment;
+            }
+        }
+        System.out.print("Минимальная сумма трат за день составила " + minPayment + " рублей. ");
+        System.out.print("Максимальная сумма трат за день составила " + maxPayment + " рублей");
+        System.out.println();
+    } // end of task
 
 } // end of main
